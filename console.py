064b4bfaf80dab769c067e923f4dff9b74bc12c2
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-03-12 10:16:43 +0100

    Create console.py

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Alx Simple command processor"""
+
+    prompt = '(hbnb)'
+    intro = 'Welcome to Airbnb,how may I help you'
+
+    def do_quit(self,line):
+        """Quits the program"""
+        quit()
+
+    def do_greet(self,person):
+        """Greets person"""
+        print("Hi, "+ person)
+
+    def do_EOF(self,line):
+        return True
+    
+    def emptyline(self):
+        return
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
