29538c78f15956eaf625dcd360043ce3a28dfe54
opxzy22 <aopeyem84@gmail.com>
2023-03-14 12:49:08 +0000

    init

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Module for Amenity class."""
+"""This module inherits from BaseModel"""
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Class representing a Amenity."""
+    """
+        Amenity class
+    """
+
     name = ""
