fcfe4f0cd00ebf1f6038ff50bfe96f5f5affd282
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-03-14 06:02:55 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 #!/bin/env python3
+"""__init__ magic method for models directory"""
 from engine.file_storage import FileStorage
 
-"""
-Defining a universal instance of FileStorage for my program
-"""
 
 storage = FileStorage()
 storage.reload
